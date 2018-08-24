b74d25cec0daf00bc2031c5f99b35adf5af22d27
Skayo <jonasg.cool@gmail.com>
2018-08-24 16:10:38 +0200

    Added logging

--- server.js
+++ server.js
@@ -10,6 +10,9 @@ const config = JSON.parse(fs.readFileSync((fs.existsSync('config.json') ? 'confi
 const db = mysql.createConnection(config);
 db.connect();
 
+if(!config.consoleOutput)
+	console.log = console.error = function() {};
+
 function storeNewEventData(webhookKey, eventData) {
 	db.query(queries.setEventData, [JSON.stringify(eventData), webhookKey], function (error, result, fields) {
 		if (error) throw error;
@@ -17,20 +20,20 @@ function storeNewEventData(webhookKey, eventData) {
 }
 
 function executeWebhook(webhookData, webhookVars, webhookCallback) {
-	for(let variable in webhookVars) {
+	for (let variable in webhookVars) {
 		webhookData.url = webhookData.url.replace(new RegExp('\\{ *' + variable + ' *\\}', 'g'), webhookVars[variable]);
 		webhookData.body = webhookData.body.replace(new RegExp('\\{ *' + variable + ' *\\}', 'g'), webhookVars[variable]);
 	}
 
 	let requestOptions = {
-		url: webhookData.url,
-		method: webhookData.method,
+		url:     webhookData.url,
+		method:  webhookData.method,
 		headers: {
 			'User-Agent': 'devRant-Webhooks'
 		}
 	};
 
-	switch(webhookData.contentType) {
+	switch (webhookData.contentType) {
 		case 'application/x-www-form-urlencoded':
 			requestOptions.form = webhookData.body;
 			break;
@@ -58,59 +61,84 @@ function newRant() {
 }
 
 function newCommentOnRant() {
-	// Check for a new rant
+	console.log('\n> Cronjob "newCommentOnRant" fired.');
+
+	console.log('> Fetching Webhooks with event-type "newCommentOnRant"...');
 	db.query(queries.getWebhooks, 'newCommentOnRant', function (error, results, fields) {
 		let errors = [];
 
-		if (error) errors.push(error);
-		else {
+		if (error) {
+			errors.push(error);
+			console.error('! An error occurred while fetching Webhooks: ' + error);
+		} else {
+			console.log('> Done.');
+
 			for (let row of results) {
+				console.log('\n> Checking Webhook with key "' + row.webhookKey + '"...');
+
 				let eventData = JSON.parse(row.eventData);
 
+				console.log('> Fetching Rant...');
 				devRant
 					.rant(eventData.rantID)
 					.then((response) => {
+						console.log('> Done.');
 						let newComments = [];
 
+						console.log('> Checking for new comments...');
 						for (let comment of response.comments) {
 							if (comment.created_time > eventData.lastTime) {
 								newComments.push(comment);
 							}
 						}
+						console.log('> Found ' + newComments.length + ' new comments.');
 
-						console.log({ newComments });
 						for (let comment of newComments) {
-							if(eventData.byUser) {
+							console.log('\n> Comment ' + comment.id);
+
+							if (eventData.byUser) {
+								console.log('> Checking if comment posted by specified user...');
+
 								let users = eventData.byUser.split(',');
 
 								let byUserWroteComment = false;
-								for(let user of users) {
-									if(user.trim() === comment.user_username) {
+								for (let user of users) {
+									if (user.trim() === comment.user_username) {
 										byUserWroteComment = true;
 									}
 								}
 
-								if(!byUserWroteComment) {
+								if (!byUserWroteComment) {
+									console.log('> Specified user didn\'t post this comment. Skipping...');
 									// If user didn't write comment then skip it
 									continue;
 								}
+
+								console.log('> Comment was posted by specified user.');
 							}
 
 							if (!comment.user_dpp) {
 								comment.user_dpp = 0;
 							}
 
+							console.log('> Executing Webhook...');
 							executeWebhook(row, comment, function (error, response, body) {
 								if (error) {
 									errors.push(error);
+									console.error('! Error while executing Webhook with key "' + row.webhookKey + '": ' + error);
+								} else {
+									console.log('> Webhook with key "' + row.webhookKey + '" successfully executed.');
 								}
 							});
 						}
 
-						if (response.comments.length > 0)
+						console.log('> Updating Webhook data...');
+						if (response.comments.length > 0) {
 							eventData.lastTime = response.comments[response.comments.length - 1].created_time;
+						}
 
 						storeNewEventData(row.webhookKey, eventData);
+						console.log('> Updated.');
 					});
 			}
 		}
